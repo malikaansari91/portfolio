a2b9ed0d808a063a1a040815a5d1acf3d8b2700d
Malika Ansari <malikaansari91@gmail.com>
2021-12-12 17:05:20 +0530

    Initial Setup

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+// import Navigation from './Navigation'
+// import Footer from './Footer'
+const Layout = ({children}) => {
+  return (
+    <>
+      {/* <Navigation /> */}
+      <main className="pt-16">{children}</main>
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default Layout
---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import {ThemeProvider} from 'next-themes'
+import Layout from '../components/Layout';
+
+function MyApp({ Component, pageProps }) {
+  return(<ThemeProvider attribute="class"><Layout><Component {...pageProps} /></Layout></ThemeProvider>);
+}
+
+export default MyApp
